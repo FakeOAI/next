b531ccd4bb8cb968c4f600d637a5a7d0a3ca8a82
fakeoai <1727283040@qq.com>
2024-08-13 18:03:40 +0000

    版本号：xtBkY11TH4yPl_7qi4aok

---
+++ _next/static/chunks/6368.3e524096eaf4e937.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[6368],{36368:function(t,e,n){n.d(e,{k:function(){return i}});let r=["https:","mailto:","tel:"],i=t=>{let e;if(void 0!==t){try{e=new URL(t,window.location.origin)}catch(t){return}if(!r.includes(e.protocol))return;return e.href}}}}]);
+//# sourceMappingURL=6368.3e524096eaf4e937.js.map
